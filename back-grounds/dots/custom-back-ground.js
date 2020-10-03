b27ed93a5b6e267d715c3e6ef8d4215da3e4cd93
Alexander <sda4000@gmail.com>
2020-10-03 13:26:41 +0300

    Add files via upload

---
+++ back-grounds/dots/custom-back-ground.js
@@ -0,0 +1,12 @@
+VANTA.GLOBE({
+  el: "#custom-back-ground",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0xbb3,
+  size: 2.00
+})
